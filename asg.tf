0eec10ffee10040b64cbf2c1b36b6092bc871d35
KalluruS <>
2020-12-30 08:43:16 +0530

    Update terraform-project/terraform-catalogue/

--- asg.tf
+++ asg.tf
@@ -8,3 +8,16 @@ module "asg" {
   PORT            = 8080
   HEALTH          = "/health"
 }
+
+resource "aws_lb_listener_rule" "catalogue" {
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_LISTENER_ARN
+  action {
+    type = "forward"
+    target_group_arn = module.asg.TG_ARN
+  }
+  condition {
+    host_header {
+      values = ["catalogue-${var.ENV}.devopsb53.tk"]
+    }
+  }
+}
