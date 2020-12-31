d845cca6f6da1591a8987564e5174eb9f01f8f22
KalluruS <>
2020-12-31 08:10:25 +0530

    Update terraform-project/terraform-catalogue/

--- asg.tf
+++ asg.tf
@@ -10,15 +10,22 @@ module "asg" {
 }
 
 resource "aws_lb_listener_rule" "catalogue" {
-  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_LISTENER_ARN
-  action {
-    type = "forward"
-    target_group_arn = module.asg.TG_ARN
+  listener_arn          = data.terraform_remote_state.alb.outputs.PRIVATE_ALB_LISTENER_ARN
+  action                  {
+    type                = "forward"
+    target_group_arn    = module.asg.TG_ARN
   }
-  condition {
-    host_header {
-      values = ["catalogue-${var.ENV}.devopsb53.tk"]
+  condition               {
+    host_header           {
+      values            = ["catalogue-${var.ENV}.devopsb53.tk"]
     }
   }
 }
 
+resource "aws_route53_record" "alb" {
+  zone_id               = "Z01648193972APZOIN217"
+  name                  = "${var.COMPONENT}-${var.ENV}.devopsb53.tk"
+  type                  = "CNAME"
+  ttl                   = "300"
+  records               = [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_DNS]
+}
